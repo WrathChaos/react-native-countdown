26a142f22f5d763b1d59275a70e15f13f3836ae3
FreakyCoder <kurayogun@gmail.com>
2023-05-09 21:44:28 +0300

    feat: countdown is modern functional component now and bug is fixed

--- example/App.tsx
+++ example/App.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import {StatusBar, SafeAreaView} from 'react-native';
-import Countdown from '@freakycoder/react-native-countdown';
+import {View} from 'react-native';
+// import Countdown from '@freakycoder/react-native-countdown';
+import Countdown from './lib/Countdown';
 import moment from 'moment';
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView
-        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Countdown
-          start={moment(1578247689000)}
-          end={moment(1578557635000)}
-          defaultCountdown=" - / - : - : -"
-          format="d[d]  hh:mm:ss"
-        />
-      </SafeAreaView>
-    </>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Countdown
+        start={moment(1578247689000)}
+        end={moment(1578557635000)}
+        defaultCountdown=" - / - : - : -"
+        format="d[d]  hh:mm:ss"
+      />
+    </View>
   );
 };
 
--- lib/Countdown.tsx
+++ lib/Countdown.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Text } from "react-native";
+import { Text, TextStyle, StyleProp, ViewStyle } from "react-native";
 import moment from "moment";
 const momentDurationFormatSetup = require("moment-duration-format");
 momentDurationFormatSetup(moment);
@@ -8,85 +8,68 @@ momentDurationFormatSetup(moment);
  */
 import styles from "./Countdown.style";
 
-export interface ICountdownProps {
+interface CountdownProps {
   end: moment.Moment;
   start: moment.Moment;
   format?: string;
-  textStyle?: any;
   defaultCountdown?: string;
+  style?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>;
   onCountdownOver?: () => void;
 }
 
-interface IState {
-  countdown: string;
-  hours: string;
-  minutes: string;
-  seconds: string;
-  timer: any;
-}
+const Countdown: React.FC<CountdownProps> = ({
+  style,
+  textStyle,
+  start,
+  end,
+  format = "hh:mm:ss",
+  onCountdownOver,
+  defaultCountdown = "- : - : -",
+  ...rest
+}) => {
+  let interval;
 
-export default class Countdown extends React.Component<
-  ICountdownProps,
-  IState
-> {
-  interval: any = null;
+  const [hours, setHours] = React.useState<string>();
+  const [minutes, setMinutes] = React.useState<string>();
+  const [seconds, setSeconds] = React.useState<string>();
+  const [countdown, setCountdown] = React.useState<string>();
+  const [timer, setTimer] = React.useState();
 
-  constructor(props: ICountdownProps) {
-    super(props);
-    this.state = {
-      hours: "",
-      minutes: "",
-      seconds: "",
-      timer: undefined,
-      countdown: props.defaultCountdown || "- : - : -",
-    };
-  }
-
-  componentDidMount() {
-    const {
-      start = moment(),
-      end = moment(),
-      format = "hh:mm:ss",
-    } = this.props;
-    this.interval = setInterval(() => {
+  React.useEffect(() => {
+    interval = setInterval(() => {
       const countDownStart = start.add(1, "second");
       const then = moment(countDownStart).format("DD/MM/YYYY HH:mm:ss");
       const now = moment(end).format("DD/MM/YYYY HH:mm:ss");
       const ms = moment(now, "DD/MM/YYYY HH:mm:ss").diff(
         moment(then, "DD/MM/YYYY HH:mm:ss"),
       );
-      const duration = moment.duration(ms);
-      const countdown = duration.format(format);
+      const _countdown = moment.duration(ms).format(format);
 
       if (ms <= 0) {
-        this.setState({ hours: "00", minutes: "00", seconds: "00" });
-        clearInterval(this.interval);
+        setHours("00");
+        setMinutes("00");
+        setSeconds("00");
+        clearInterval(interval);
       } else {
-        this.setState({ countdown: countdown });
+        setCountdown(_countdown);
       }
     }, 1000);
-  }
+  }, []);
 
-  componentDidUpdate() {
-    if (this.state.timer === 1) {
-      const { onCountdownOver } = this.props;
-      onCountdownOver && onCountdownOver();
-      clearInterval(this.interval);
-      this.forceUpdate();
+  React.useEffect(() => {
+    if (timer === 1) {
+      clearInterval(interval);
+      onCountdownOver?.();
+      // this.forceUpdate();
     }
-  }
+  }, [timer]);
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+  return (
+    <Text style={[styles.textStyle, textStyle]} {...rest}>
+      {countdown}
+    </Text>
+  );
+};
 
-  render() {
-    const { textStyle } = this.props;
-    const { countdown } = this.state;
-    return (
-      <Text style={[styles.textStyle, textStyle]} {...this.props}>
-        {countdown}
-      </Text>
-    );
-  }
-}
+export default Countdown;
